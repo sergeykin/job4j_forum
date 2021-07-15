1f9a9ebcc39847036c6019911f07f3d12eb3089b
sergeykin <sergeykinn@mail.ru>
2021-07-15 22:00:00 +0300

    0. Spring test [#6881 #177394]

--- src/test/java/ru/job4j/forum/control/IndexControlTest.java
+++ src/test/java/ru/job4j/forum/control/IndexControlTest.java
@@ -8,12 +8,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -22,9 +29,15 @@ public class IndexControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PostService postService;
+
     @Test
     @WithMockUser
     public void shouldReturnDefaultMessage() throws Exception {
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post());
+        Mockito.when(postService.getAll()).thenReturn(posts);
         this.mockMvc.perform(get("/index"))
                 .andDo(print())
                 .andExpect(status().isOk())
