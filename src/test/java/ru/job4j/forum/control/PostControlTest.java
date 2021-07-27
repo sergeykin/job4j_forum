31b453eba1d97cb2c6eeaba65d04b2e076fbc927
sergeykin <sergeykinn@mail.ru>
2021-07-27 10:46:35 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,13 +1,17 @@
 package ru.job4j.forum.control;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
 import ru.job4j.forum.model.Post;
 import ru.job4j.forum.service.PostService;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -56,4 +61,17 @@ public class PostControlTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("post"));
     }
+
+    @Test
+    @WithMockUser
+    public void save() throws Exception {
+        this.mockMvc.perform(post("/save")
+                .param("name","Куплю ладу-грант. Дорого."))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(postService).add(argument.capture());
+        assertThat(argument.getValue().getName(), is("Куплю ладу-грант. Дорого."));
+    }
+
 }
